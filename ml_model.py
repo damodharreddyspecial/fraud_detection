a0812015dc81c3a919686570e76c52396e21d26a
damodharreddyspecial <damodharreddyspecial@outlook.com>
2022-01-29 21:12:48 +0530

    added 3 new ML model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,11 @@
 print("adding all ml related code")
+
+def model1():
+    print("adding code for ML Model1")
+
+def model2():
+    print("adding code for ML Model2")
+
+def model3():
+    print("adding code for ML Model3")
+
